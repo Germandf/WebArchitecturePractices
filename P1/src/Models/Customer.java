db255eff6148c1233cb02be5513fffa5922002b1
German De Francesco <germandefrancesco@hotmail.com>
2022-09-20 19:30:13 -0300

    Fix db connection and entity tags missing

--- P1/src/Models/Customer.java
+++ P1/src/Models/Customer.java
@@ -1,8 +1,19 @@
 package Models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+	@Column
     private String name;
+	@Column
     private String email;
 
     public Customer(int id, String name, String email) {
